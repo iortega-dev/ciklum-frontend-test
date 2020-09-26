bab341ca9f0ab8d50c4182670845aa1786b2ccbe
iortega-dev <iortegamancera@gmail.com>
2020-09-26 20:13:12 +0200

    [MOD] Refactor Jackpot date picker

---
+++ src/components/JackpotDatepicker/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { PickersContainer, Select } from './styled'
+import classnames from 'classnames'
+
+const JackpotDatepicker = () => {
+  return (
+    <PickersContainer
+      className={classnames(
+        'row',
+        'no-gutters',
+        'col-xs-12',
+        'col-md-auto',
+        'justify-content-around'
+      )}
+    >
+      <Select
+        className={classnames('col-xs-12', 'col-sm-5', 'col-md-auto')}
+        name="date-datepicker"
+        id="date-datepicker"
+      >
+        <option>Fri 25 Sep</option>
+      </Select>
+      <Select
+        className={classnames('col-xs-12', 'col-sm-5', 'col-md-auto')}
+        name="year-datepicker"
+        id="year-datepicker"
+      >
+        <option>2020</option>
+      </Select>
+    </PickersContainer>
+  )
+}
+
+export default JackpotDatepicker
--- src/screens/Home/styled.ts
+++ src/screens/Home/styled.ts
@@ -24,27 +24,3 @@ export const H1 = styled.h1`
     text-align: left;
   }
 `
-
-export const PickersContainer = styled.div`
-  select {
-    margin-bottom: ${(props) => props.theme.spacing[1]};
-    margin-left: 0;
-    margin-right: 0;
-  }
-  @media (${(props) => props.theme.breakpoints.sm}) {
-    select {
-      margin-bottom: 0;
-      margin-left: ${(props) => props.theme.spacing[1]};
-    }
-  }
-`
-
-export const Select = styled.select`
-  border-color: ${(props) => props.theme.colors.borderGrey};
-  border-radius: 3px;
-  color: ${(props) => props.theme.colors.textPrimary};
-  font-family: ${(props) => props.theme.fonts[0]};
-  font-size: ${(props) => props.theme.fontSizes.medium};
-  font-weight: 300;
-  padding: ${(props) => props.theme.spacing['0.5']};
-`
