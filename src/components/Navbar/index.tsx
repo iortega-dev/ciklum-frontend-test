7d5bf135413373c5e882da9ba8469ed047628d21
iortega-dev <iortegamancera@gmail.com>
2020-09-26 18:42:35 +0200

    [ADD] Used translation at Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 // Logo asset
 import logo from '~Assets/images/ll-logo-green.svg'
@@ -6,10 +7,12 @@ import logo from '~Assets/images/ll-logo-green.svg'
 import { Section, Header, Img } from './styled'
 
 const Navbar = () => {
+  const { t } = useTranslation()
+  
   return (
     <Section>
       <Header className="container">
-        <Link to="/" title="Play Lotto Online and win Millions at Lottoland.com">
+        <Link to="/" title={t('Header.LogoTitle')}>
           <Img src={logo} alt="Lottoland"/>
         </Link>
       </Header>
