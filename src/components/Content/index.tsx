4a8d9d73314b2b5605813ef77f05b94ca3ace9b2
iortega-dev <iortegamancera@gmail.com>
2020-09-26 17:08:14 +0200

    [ADD] Added basic Content component

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// Routes definition
+import Routes from '~Routes/index'
+
+const Content = () => {
+  return (
+    <div>
+       <Routes />
+    </div>
+  )
+}
+
+export default Content
