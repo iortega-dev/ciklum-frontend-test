90c11eb7be6e9ede59d9f9eff0fce73c5e0d2f69
iortega-dev <iortegamancera@gmail.com>
2020-09-26 21:39:00 +0200

    [ADD] Added JackpotResults to Homescreen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,17 +5,19 @@ import classnames from 'classnames'
 import { Container, H1, Header } from './styled'
 
 import JackpotDatepicker from '~Components/JackpotDatepicker'
+import JackpotResults from '~Components/JackpotResults'
 
 const HomeScreen = () => {
   const { t } = useTranslation()
   return (
     <Container className="container">
-      <Header className="row">
+      <Header className={classnames('row', 'no-gutters')}>
         <H1 className={classnames('col-xs-12', 'col-md-auto')}>
           {t('EuroJackpot.Title')}
         </H1>
         <JackpotDatepicker />
       </Header>
+      <JackpotResults />
     </Container>
   )
 }
