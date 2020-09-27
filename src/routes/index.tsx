986d550f456cec577cdf4beb154f44ac0cd97dc6
iortega-dev <iortegamancera@gmail.com>
2020-09-27 17:14:14 +0200

    [ADD] Implemented 404 Screen to Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
+// Screens import
+import NotFoundScreen from '~Screens/404'
 import EuroJackpotScreen from '~Screens/Eurojackpot'
 
 const Routes = () => {
@@ -8,6 +10,7 @@ const Routes = () => {
       <Route exact path="/" render={() => <Redirect to="/eurojackpot" />} />
       <Route path="/eurojackpot/:drawndate" component={EuroJackpotScreen} />
       <Route path="/eurojackpot" component={EuroJackpotScreen} />
+      <Route component={NotFoundScreen}/>
     </Switch>
   )
 }
