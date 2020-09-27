c2f198e074e6ce8f1628ca73dffdf1a2d23fc766
iortega-dev <iortegamancera@gmail.com>
2020-09-27 16:21:20 +0200

    [MOD] Refactor JackpotDraw state

--- src/screens/Eurojackpot/index.tsx
+++ src/screens/Eurojackpot/index.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import classnames from 'classnames'
 import moment from 'moment'
 import { useHistory, useParams } from 'react-router-dom'
 
-import { Container, H1, Header } from './styled'
+// Styled components
+import { BackLink, Container, H1, H2, Header } from './styled'
 
-import { Last } from '~Common/interfaces/Jackpot'
-
+// Local imports
 import JackpotDatepicker from '~Components/Jackpot/JackpotDatepicker'
 import JackpotResults from '~Components/Jackpot/JackpotResults'
 import { Loading } from '~Components/Common/Loading'
-
-import { getEurojackpotResults } from '~Api'
+// Custom hooks
+import { useJackpotDraw } from '~Hooks/useJackpotDraw'
 
 interface ParamTypes {
   drawndate: string
@@ -22,36 +22,8 @@ const EuroJackpotScreen = () => {
   const { t } = useTranslation()
   const { drawndate } = useParams<ParamTypes>()
   const history = useHistory()
-  
-  const [error, setError] = useState<string | undefined>(undefined)
-  const [showLoading, setShowLoading] = useState(false)
-  const [jackpotResults, setJackpotResults] = useState<Last | undefined>(undefined)
-  
-  useEffect(() => {
-    setShowLoading(true)
-    getEurojackpotResults(moment(drawndate, 'DD-MM-YYYY').format('YYYYMMDD'))
-      .then(({ data }) => {
-        if (Array.isArray(data.last) && data.last.length > 0) {
-          setError(undefined)
-          setJackpotResults(data.last[0])
-        } else if (Array.isArray(data.last) && data.last.length === 0 ) {
-          setError("Sorry, there are no draw results for the day indicated.")
-          setJackpotResults(undefined)
-        }
-        else {
-          setError(undefined)
-          setJackpotResults(data.last as Last)
-        }
-      })
-      .catch((error) => {
-        if (error.response) {
-          setError("Sorry, there was an error while trying to retrieve the results.");
-        } else if (error.request) {
-          setError("Sorry, there was an error while connecting to the server.")
-        }
-      })
-      .finally(() => setShowLoading(false))
-  }, [drawndate])
+
+  const {jackpotResults, showLoading, error} = useJackpotDraw(drawndate)
 
   const dayChange = (day: Date) => {
     const formatedDate = moment(day).format('DD-MM-YYYY')
@@ -71,7 +43,10 @@ const EuroJackpotScreen = () => {
         <JackpotResults jackpotResults={jackpotResults} />
       )}
       {!showLoading && error && !jackpotResults && (
-        <h1>{error}</h1>
+        <>
+          <H2>{error}</H2>
+          <BackLink to="/">{t('EuroJackpot.Error.BackButton')}</BackLink>
+        </>
       )}
     </Container>
   )
