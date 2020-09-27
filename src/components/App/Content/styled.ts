a6a70b64283e3473091e577461303301d1065c77
iortega-dev <iortegamancera@gmail.com>
2020-09-27 02:15:37 +0200

    [MOD] Modified Navbar styles

--- src/components/App/Content/styled.ts
+++ src/components/App/Content/styled.ts
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const ContentSection = styled.section`
   padding-top: 70px;
+  margin-bottom: 70px;
 `
--- src/components/App/Navbar/styled.ts
+++ src/components/App/Navbar/styled.ts
@@ -3,6 +3,9 @@ import styled from "styled-components"
 export const Section = styled.section`
   width: 100%;
   position: fixed;
+  background: ${(props) => props.theme.colors.white};
+  z-index: 1;
+  border-bottom: 1px solid ${(props) => props.theme.colors.borderGrey};
 `
 
 export const Header = styled.header`
