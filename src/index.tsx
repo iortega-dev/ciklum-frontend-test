0ee1bc5b80b109ded8933dc87d244e89244b0e99
iortega-dev <iortegamancera@gmail.com>
2020-09-27 14:04:04 +0200

    [ADD] Import react-day-picker css file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import '~Assets/css/bootstrap-grid.css';
 // Super Responsive table
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+// React Day Picker
+import 'react-day-picker/lib/style.css';
 // Internationalization
 import './i18n.ts'
 // Global styles
